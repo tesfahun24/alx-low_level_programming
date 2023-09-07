14d796a702b9e434fb490996232d5b1f95831d2b
tesfahun24 <tesfahunalemayehu71@gmail.com>
2023-09-07 04:01:47 -0700

    pust done

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,6 @@
-#include <stdio.h>
-
-/**
-  * main - print the string in the put function
-  *
-  * Description: using the main function
-  * this program prints "Programming is like building a multilingual puzzle
-  * Return: 0
-  */
+#include<stdio.h>
+/*This is the main function. It prints the string "Programming is like building a multilingual puzzle" to the standard output.
+ */
  int main(void) 
 {
 	 puts("\"Programming is like building a multilingual puzzle");
