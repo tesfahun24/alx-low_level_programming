39e39576738886d1af9b8df7c07c6a1dff79ceea
tesfahun24 <tesfahunalemayehu71@gmail.com>
2023-09-07 03:56:03 -0700

    puts.c done

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,8 +7,9 @@
   * this program prints "Programming is like building a multilingual puzzle
   * Return: 0
   */
+
 int main(void) 
 {
 	 puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
