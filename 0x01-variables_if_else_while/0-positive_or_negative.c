978133b9db404dcc90144e2efe4265f9df0d533d
tesfahun24 <tesfahunalemayehu71@gmail.com>
2023-09-08 04:50:47 -0700

    adding nagative posetive filter

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,29 +1,26 @@
 #include<stdio.h>
 #include<time.h>
 #include<stdlib.h>
-/* headers goes there */
-
 /**
- * main - prints whether a random number is postive
-  * or negative
-   * Return: 0
-*/ 
+  *main- prints
+  * or nagative
+  *Return: 0
+  */
 int main(void)
 {
-	int n;
-	srand(time(0)); 
-
-	n = rand() - RAND_MAX / 2;
-	n = n % 100;
-		/* your code goes there */
-	if (n > 0)
-	printf("%02d is positive\n", n);
-	else if (n < 0)
-	printf("%02d is negative\n", n);
-	else
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+n = n % 100;
+/* your code goes there */
+if (n > 0)
+printf("%02d is positive\n", n);
+else if (n < 0)
+printf("%02d is negative\n", n);
+else
 {
-	n = n % 10;
-	printf("%d is zero\n", n);
+n = n % 10;
+printf("%d is zero\n", n);
 }
 return (0);
 }
