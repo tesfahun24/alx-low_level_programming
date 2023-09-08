83ad491c499adc5e38ce47dc0cdcdf0e6d713ca5
tesfahun24 <tesfahunalemayehu71@gmail.com>
2023-09-08 06:37:47 -0700

    print Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+*main-prints numbers from 0 to 10
+*Return: o exits main function
+*/
+int main(void)
+{
+int i = 0;
+for (; i < 10; i++)
+printf("%i", i);
+printf("\n");
+return (0);
+}
+
