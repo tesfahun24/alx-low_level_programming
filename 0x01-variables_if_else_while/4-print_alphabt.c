aea32af0ce43500c4d201e8f0dab8100b82b6f35
tesfahun24 <tesfahunalemayehu71@gmail.com>
2023-09-08 06:23:50 -0700

    Alphabets except 'e' and 'q'

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,9 +8,15 @@ int main(void)
 char ch = 'a';
 while (ch <= 'z')
 {
-if (ch != 'q' || ch != 'e')
+if (ch == 'e' || ch == 'q')
+{
+ch++;
+}
+else
+{
 putchar(ch);
 ch++;
+}
 }
 putchar('\n');
 return (0);
