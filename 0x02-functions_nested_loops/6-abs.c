f2605dfc3ed1cdbdc74e3ebc3a15a3193bdbfd0b
tesfahun24 <tesfahunalemayehu71@gmail.com>
2023-09-18 05:14:27 -0700

    absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* int _abs - check the absolute value
+*@a: Number to be checked
+* Return: Always positive.
+*/
+int _abs(int a)
+{
+if (a >= 0)
+{
+return (a);
+}
+else
+{
+a = a * ((2 * a + 1) % 2);
+return (a);
+}
+}
