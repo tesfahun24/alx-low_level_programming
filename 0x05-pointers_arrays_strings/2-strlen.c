65d7dc6864c6e8c8d95e95329602147aab1472c7
tesfahun24 <tesfahunalemayehu71@gmail.com>
2023-09-18 08:25:20 -0700

    pointer

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* _strlen - check the code for ALX  students.
+* @s: declaration of *s and paramters for the function _strlen
+* Return: Always 0.
+*/
+int _strlen(char *s)
+{
+int c;
+for (c = 0; s[c] != 0; c++)
+{
+}
+return (c);
+}
