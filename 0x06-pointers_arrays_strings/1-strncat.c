71356550834f806bf1f1209747942cb482140c27
tesfahun24 <tesfahunalemayehu71@gmail.com>
2023-09-20 08:00:03 -0700

    Concatenate n string

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+** _strncat -  function that concatenates two strings.
+* @dest: -  The string
+* @src: - The string
+ * @n: - The integer
+ * Return:  concatenated string
+*/
+char *_strncat(char *dest, char *src, int n)
+{
+
+   int dest_len = 0, i = 0;
+while (dest[dest_len] != '\0')
+{
+dest_len++;
+}
+while (i < n && src[i] != '\0') {
+dest[dest_len] = src[i];
+i++;
+dest_len++;
+}
+return ((char *)dest);
+}
