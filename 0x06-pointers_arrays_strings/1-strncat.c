457ee8d8682c513b7a03aa246523dc2e36da74e8
tesfahun24 <tesfahunalemayehu71@gmail.com>
2023-09-20 08:13:37 -0700

    checked

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -8,16 +8,15 @@
 */
 char *_strncat(char *dest, char *src, int n)
 {
-
    int dest_len = 0, i = 0;
 while (dest[dest_len] != '\0')
 {
 dest_len++;
 }
 while (i < n && src[i] != '\0') {
-dest[dest_len] = src[i];
-i++;
+*(dest + dest_len) = *(src + i);
 dest_len++;
+i++;
 }
-return ((char *)dest);
+return (dest);
 }
