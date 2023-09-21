f25ba87e927877c491d49c0720ea63f378405cc0
tesfahun24 <tesfahunalemayehu71@gmail.com>
2023-09-21 00:36:18 -0700

    compare two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,35 +1,24 @@
 #include "main.h"
 /**
-* *_strncmp - a function that compares  a string.
+* *_strcmp - a function that compares  a string.
 * @s1: input one
 * @s2: input two
 * Return: Always 0.
 */
-int _strncmp(char *s1, char *s2)
+int _strcmp(char *s1, char *s2)
 {
 int i = 0;
 while (s1[i] != '\0' && s2[i] != '\0')
 {
 if (s1[i] < s2[i])
 {
-return (-1);
+return (s1[i] - s2[i]);
 }
 else if (s1[i] > s2[i])
 {
-return (1);
+return (s1[i] - s2[i]);
 }
 i++;
 }
-if (s1[i] == '\0' && s2[i] != '\0')
-{
-return (-1);
-}
-else if (s1[i] != '\0' && s2[i] == '\0')
-{
-return (1);
-}
-else
-{
 return (0);
-}
 }
